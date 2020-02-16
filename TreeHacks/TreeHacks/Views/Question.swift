b5df53b866c21f68842df015dfa7d5d829a6bb70
SophieMBerger <berger.m.sophie@gmail.com>
2020-02-16 06:33:50 -0800

    fix

--- TreeHacks/TreeHacks/Views/Question.swift
+++ TreeHacks/TreeHacks/Views/Question.swift
@@ -11,9 +11,6 @@ import SwiftUI
 struct Question: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        HStack {
-            
-        }
     }
 }
 
